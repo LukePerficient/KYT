d87af7b8070ed56e6752f89fb3c83c437061c475
DDC.Mac2 <ddc.mac2@ddc-mac2-mac.local>
2017-09-12 21:13:50 -0500

    final push

---
+++ Pods/Target Support Files/Pods-KnowYourTeam/Pods-KnowYourTeam-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KnowYourTeam : NSObject
+@end
+@implementation PodsDummy_Pods_KnowYourTeam
+@end
---
+++ Pods/Target Support Files/Pods-KnowYourTeamTests/Pods-KnowYourTeamTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KnowYourTeamTests : NSObject
+@end
+@implementation PodsDummy_Pods_KnowYourTeamTests
+@end
