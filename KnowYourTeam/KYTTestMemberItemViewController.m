6608a74bed426a3ed07aaad47bb6eafac6e1854e
DDC.Mac1 <ddc.mac1@ddc-mac1-mac.local>
2017-09-19 15:50:21 -0500

    Updated answer text with style

--- KnowYourTeam/KYTTestMemberItemViewController.m
+++ KnowYourTeam/KYTTestMemberItemViewController.m
@@ -25,14 +25,26 @@
 - (IBAction)checkUserAnswer:(UIButton *)sender {
     if ([_member.firstName isEqualToString:_memberTestTextField.text]) {
         _answerTextField.textColor = [UIColor greenColor];
+        _answerTextField.layer.borderColor = [[UIColor greenColor]CGColor];
+        _answerTextField.layer.borderWidth = 2.0f;
+        _answerTextField.layer.cornerRadius = 5;
+        _answerTextField.clipsToBounds = YES;
         _answerTextField.text = @"Correct";
         
-        NSLog(@"Correct");
+        //NSLog(@"Correct");
     } else {
         _answerTextField.textColor = [UIColor redColor];
+        _answerTextField.layer.borderColor = [[UIColor redColor]CGColor];
+        _answerTextField.layer.borderWidth = 2.0f;
+        _answerTextField.layer.cornerRadius = 5;
+        _answerTextField.clipsToBounds = YES;
         _answerTextField.text = @"Incorrect";
-        NSLog(@"Incorrect");
+        //NSLog(@"Incorrect");
+        
     }
+    
+    //Add code to pause for 1 sec, then move onto next slide.
+    //Add code to disallow user from going back
 }
 
 // MARK: Private Methods
